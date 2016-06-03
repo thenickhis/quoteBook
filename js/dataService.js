4295152f1326d0436b708a9ce4d546db09379ffc
thenickhis <thenickhis@gmail.com>
2016-06-02 20:26:29 -0600

    first

---
+++ js/dataService.js
@@ -0,0 +1,5 @@
+angular
+	.module("quoteBook")
+	.service('dataService', function () {
+		
+	});
---
+++ js/mainCtrl.js
@@ -0,0 +1,5 @@
+angular
+	.module("quoteBook")
+	.controller('mainCtrl', function ($scope) {
+		
+	})
