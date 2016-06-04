e42a73e3a8b5ef7ed79709976162f4864c8b76e8
thenickhis <thenickhis@gmail.com>
2016-06-04 12:17:02 -0600

    add, delete, search

--- js/dataService.js
+++ js/dataService.js
@@ -1,5 +1,30 @@
-angular
-	.module("quoteBook")
-	.service('dataService', function () {
-		
+angular.module("quoteBook")
+	.factory('dataService', function () {
+    ///Variables///
+		var service = {};
+		var quoteData = [
+	    { text: 'Life isn\'t about getting and having, it\'s about giving and being.', author: 'Kevin Kruse'},
+	    { text: 'Whatever the mind of man can conceive and believe, it can achieve', author: 'Napoleon Hill'},
+	    { text: 'Strive not to be a success, but rather to be of value.', author: 'Albert Einstein'},
+	    { text: 'Two roads diverged in a wood, and I took the one less traveled by, And that has made all the difference.', author: 'Robert Frost'},
+	    { text: 'The most difficult thing is the decision to act, the rest is merely tenacity.', author: 'Amelia Earhart'},
+    	{ text: 'Life is what happens to you while you\'re busy making other plans.', author: 'John Lennon'},
+    	{ text: 'What even is a jQuery?', author: 'Tyler S. McGinnis'}
+  	];
+
+    ///Methods///
+  	service.getData = function() {
+  		return quoteData;
+  	};
+  	service.addData = function(text, author) {
+  		if (text)
+        quoteData.unshift({ text: text, author: author || 'Anonymous' });
+  	};
+  	service.removeData = function(str) {
+  		quoteData = quoteData.filter(function(obj){
+  			return obj.text !== str;
+  		});
+      return quoteData;
+  	};
+    return service;
 	});
--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,5 +1,16 @@
-angular
-	.module("quoteBook")
-	.controller('mainCtrl', function ($scope) {
-		
-	})
+angular.module("quoteBook")
+	.controller('mainCtrl', function ($scope, dataService) {
+		$scope.quotes = dataService.getData();
+		$scope.addQuote = function() {
+			if ($scope.newQuote) {
+				var author = prompt("Please enter your name.");
+				dataService.addData($scope.newQuote,author);
+				$scope.newQuote = "";
+			} else {
+				alert("Please enter your quote.");
+			}
+		}
+		$scope.remove = function(str) {
+			$scope.quotes = dataService.removeData(str);
+		}
+	});
